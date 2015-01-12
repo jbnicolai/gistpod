f73754ebb4e8794f9b6399f649c565adb260abf5
bryn austin bellomy <bryn@signals.io>
2015-01-11 20:36:18 -0600

    stupid sublime text

--- gistpod.rb
+++
@@ -1,9 +0,0 @@
-
-
-require 'rubygems'
-require 'json'
-
-json = File.read(ENV['HOME'] + '/.gistpod-cache')
-pods = JSON.parse(json)
-
-::Gistpods = Hash[ pods.map {|pod| [ pod['name'], pod['raw_url'] ]} ]
